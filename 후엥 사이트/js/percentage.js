5f4f940347ed28d06dea6383fdf5805fdb7c42e4
Lee Hyeonu <zzxx373014@gmail.com>
2022-01-10 16:13:49 +0900

    percentage correction
    js. percentage correction

--- 후엥 사이트/js/percentage.js
+++ 후엥 사이트/js/percentage.js
@@ -2,6 +2,8 @@ function begin() {
   var percent = 1 - 0.01 * document.getElementById('percent').value;
   var attempts = document.getElementById('attempts').value;
   var krw = document.getElementById('krw').value;
+  var percent__1 = document.getElementById('percent').value;
+
 
   var result1 = 1 - percent ** attempts;
   var result2 = percent ** attempts;
@@ -40,7 +42,7 @@ function begin() {
   var money_1 = document.querySelector('.money_1')
   var money_2 = document.querySelector('.money_2')
   money.innerHTML = '당신의 가차비용은 "' +krw*attempts +'" 원입니다.';
-  money_1 .innerHTML = percent +'%로' + attempts+ '번 시도했을때 한개이상 나올 확률은' +result1*100 + '% 입니다.';
-money_2 .innerHTML =percent +'%로' + attempts+ '번 시도했을때 한번만 나올 확률은' +result2*100 + '% 입니다.';
+  money_1 .innerHTML = percent__1 +'%로' + attempts+ '번 시도했을때 한개이상 나올 확률은' +result1*100 + '% 입니다.';
+money_2 .innerHTML =percent__1 +'%로' + attempts+ '번 시도했을때 한번만 나올 확률은' +result2*100 + '% 입니다.';
 
 }
