f047c3233b65120457729c1aa47bb36407a5cc4d
Lee Hyeonu <zzxx373014@gmail.com>
2022-01-11 15:40:14 +0900

    community update
    community update

--- 후엥 사이트/js/community.js
+++ 후엥 사이트/js/community.js
@@ -1,8 +1,10 @@
 const main = document.querySelector("#main");
 const writing = document.querySelector("#writing");
-
-
-
+var password = document.getElementById('inputPassword2').value;
+var email = document.getElementById('email').value;
+var user = document.getElementById('user').value;
+var story = document.getElementById('story').value;
+var title = document.getElementById('title').value;
 
 function begin() {
   main.style.animation = "fadeOut 1s";
@@ -21,6 +23,9 @@ function begin() {
 
 
 function rebegin() {
+
+
+
   writing.style.animation = "fadeOut 1s";
   setTimeout(() => {
     main.style.animation = "fadeIn 1s";
@@ -28,8 +33,12 @@ function rebegin() {
     setTimeout(() => {
       writing.style.display = "none";
       main.style.display = "block";
-      var inputPassword = document.querySelector('.inputPassword');
-      console.log(inputPassword);
+      var password = document.getElementById('inputPassword2').value = "";
+      var email = document.getElementById('email').value = "";
+      var user = document.getElementById('user').value = "";
+      var story = document.getElementById('story').value = "";
+      var title = document.getElementById('title').value = "";
+
     }, 450)
 
   }, 450)
