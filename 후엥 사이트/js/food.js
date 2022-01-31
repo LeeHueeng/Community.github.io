23e42345adc65ae392ed9764826842340cfcf2cc
Lee Hyeonu <zzxx373014@gmail.com>
2022-01-31 23:59:18 +0900

    커뮤니티 추천기 완성
    커뮤니티 추천 완성 및.
    다른 사이트에서도 커뮤니티 추천 들어갈 수 있게 수정

---
+++ 후엥 사이트/js/food.js
@@ -0,0 +1,16 @@
+
+
+window.onload=function(){
+
+  for(i=1;i<7;i++){
+
+      setTimeout(() => {
+  document.getElementById("food").src = "./img/food/"+i+".png"
+      setTimeout(() => {
+console.log(i);
+}, 100)
+}, 100)
+}
+return(function());
+
+}
--- 후엥 사이트/js/start.js
+++ 후엥 사이트/js/start.js
@@ -3,16 +3,18 @@ const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 const endPoint = 12;
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const aList=["o","x"];
 
-
-function begin() {
+function begin(){
+  main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
   setTimeout(() => {
+    qna.style.WebkitAnimation = "fadeIn 1s";
     qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
       main.style.display = "none";
-      qna.style.display = "block";
-    }, 450);
+      qna.style.display = "block"
+    }, 450)
     let qIdx = 0;
     goNext(qIdx);
   }, 450);
@@ -23,78 +25,63 @@ function begin() {
 
 
 
-function goNext(qIdx ) {
-  let per = qIdx;
+
+
+function goNext(qIdx){
+  if(qIdx === endPoint){
+    goResult();
+    return;
+  }
+
   var q = document.querySelector('.qBox');
-  q.innerHTML = qnaList[per].q;
-
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+  }
   var status = document.querySelector('.statusBar');
-  status.style.width = (100 / endPoint) * (per + 1) + '%';
-  status.innerHTML = per + 1;
-
-  let qnaURL = './img/question/0';
-  let leftURL = qnaURL + '-A.png';
-  let rightURL = qnaURL + '-B.png';
-
-  let leftImage = document.querySelector('.leftImage');
-  let rightImge = document.querySelector('.rightImge');
-  leftImage.src = leftURL;
-  rightImge.src = rightURL;
-  leftImage.style.display = 'block';
-  rightImge.style.display = 'block';
-
-  leftImage.classList.remove('fadeOut');
-  rightImge.classList.remove('fadeOut');
-
-  leftImage.classList.add('fadeIn');
-  rightImge.classList.add('fadeIn');
-
-  leftImage.addEventListener("click", function() {
-    imgeNext(per, 0);
-
-  }, false);
-  return 0;
-  rightImge.addEventListener("click", function() {
-    imgeNext(per, 1);
-
-  }, false);
-  return 0;
-
+  status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
 
 
 
+function addAnswer(answerText, qIdx, idx){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  answer.classList.add('my-3');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
 
 
-function imgeNext(per, xdx) {
-  let leftImage = document.querySelector('.leftImage');
-  let rightImge = document.querySelector('.rightImge');
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
 
-  leftImage.disabled = true;
-  leftImage.classList.remove('fadeIn');
-  leftImage.classList.add('fadeOut');
-
-
-
-  rightImge.disabled = true;
-  rightImge.classList.remove('fadeIn');
-  rightImge.classList.add('fadeOut');
-
-
-  setTimeout(() => {
-    if (per + 1 === endPoint) {
-      goResult();
-
-    } else {
-      let xIdx = per;
-      let idx = xdx;
-      resultsection(xIdx, xdx);
-
+  answer.addEventListener("click", function(){
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
+      children[i].style.animation = "fadeOut 0.5s";
     }
-  }, 450)
+    setTimeout(() => {
+      var target = qnaList[qIdx].a[idx].type;
+      for(let i = 0; i < target.length; i++){
+        select[target[i]] += 1;
+      }
 
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    },450)
+  }, false);
 }
+
+
+
+
 
 
 
@@ -119,34 +106,33 @@ function resultsection(xIdx, idx) {
   }, 450)
 }
 
-
-function goResult() {
+function goResult(){
+  qna.style.WebkitAnimation = "fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
   setTimeout(() => {
+    result.style.WebkitAnimation = "fadeIn 1s";
     result.style.animation = "fadeIn 1s";
     setTimeout(() => {
       qna.style.display = "none";
       result.style.display = "block"
-    }, 450)
-  })
-  setResult();
+    }, 450)})
+    setResult();
 }
 
 
 
 
 
-
-function calResult() {
+function calResult(){
+  console.log(select);
   var result = select.indexOf(Math.max(...select));
   return result;
 }
 
-function setResult() {
+function setResult(){
   let point = calResult();
   const resultName = document.querySelector('.resultname');
   resultName.innerHTML = infoList[point].name;
-
 
   var resultImg = document.createElement('img');
   const imgDiv = document.querySelector('#resultImg');
